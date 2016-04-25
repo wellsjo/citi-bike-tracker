a63fbf2886e6e15276e1b7dae081603c5a9b5474
Wells Johnston <wellsjohnston@gmail.com>
2016-04-24 20:30:56 -0400

    can text myself using twilio:

--- config/index.js
+++ config/index.js
@@ -5,6 +5,6 @@ const config = {};
 config.authtoken = '039be0fcf0ff89c13f1d3f325a1a2630';
 config.sid = 'ACc2c16a5e83b9f3acbd8ad03c493862f4';
 
-config.number = '+14157068928';
+config.number = '+14136674180';
 
 module.exports = config;
