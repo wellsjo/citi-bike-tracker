b3e100c8c061cb5077b9575cd637c13e42a237af
Wells Johnston <wellsjohnston@gmail.com>
2016-04-27 19:21:49 -0400

    using profiles now to send out messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const config = require('./config');
+const users = require('./config/profiles.json');
 const text = require('./lib/text');
 const citibike = require('./lib/citibike');
 const schedule = require('node-schedule').scheduleJob;
@@ -8,13 +9,27 @@ const schedule = require('node-schedule').scheduleJob;
 let sent = false;
 
 schedule('* * * * *', _ => {
+
+  let now = Number([new Date().getHours(), new Date().getMinutes()].join(''));
   citibike.getSystem('citi-bike-nyc', (err, data) => {
-    let stations = data.filter(s => config.ids.indexOf(s.id) > -1);
-    let bikes = stations.reduce((p, c) => p + c.bikes, 0);
-    console.log(bikes);
-    if (bikes < 10 && !sent) {
-      text(bikes + ' bikes left downtown');
-      sent = true;
-    }
+
+    users.forEach(user => {
+
+      user.profiles.forEach(profile => {
+
+        if (profile.start > now || profile.end < now) {
+          return;
+        }
+
+        let stations = data.filter(s => profile.stations.indexOf(s.id) > -1);
+        let bikes = stations.reduce((p, c) => p + c.bikes, 0);
+
+        console.log(bikes + ' bikes left');
+        if (bikes < profile && !sent) {
+          text(bikes + ' bikes left downtown');
+          sent = true;
+        }
+      });
+    });
   });
 })
