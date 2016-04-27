c557c10a705339bdf26e61e32aab1d1ad4540d40
Wells Johnston <wellsjohnston@gmail.com>
2016-04-27 16:57:14 -0400

    fuck

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict'
 
+const config = require('./config');
 const citibike = require('./lib/citibike');
 const schedule = require('node-schedule').scheduleJob;
 
-// schedule('* * * * *', _ => {
-
-  citibike.getStations([209], (err, res) => {
-    console.log(res[0].bikes);
-  });
-
-  citibike.getSystem('citi-bike-nyc', (err, data) => {
-
-  });
-
-// });
+citibike.getSystem('citi-bike-nyc', (err, data) => {
+  let stations = data.filter(s => config.ids.indexOf(s.id) > -1);
+  let bikes = stations.reduce((p, c) => p + c.bikes, 0);
+  console.log(stations);
+  console.log(bikes);
+});
