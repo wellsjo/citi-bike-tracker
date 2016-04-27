d7becdbacb62ab13b7ddcec94a4042a6c96cc242
Wells Johnston <wellsjohnston@gmail.com>
2016-04-27 17:25:21 -0400

    created the concept of profiles

--- config/index.js
+++ config/index.js
@@ -1,11 +1,7 @@
 'use strict'
 
 const config = {};
-
 config.authtoken = '039be0fcf0ff89c13f1d3f325a1a2630';
 config.sid = 'ACc2c16a5e83b9f3acbd8ad03c493862f4';
-
-config.number = '+14136674180';
-config.ids = [301, 57, 193];
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const config = require('./config');
+const text = require('./lib/text');
 const citibike = require('./lib/citibike');
 const schedule = require('node-schedule').scheduleJob;
 
-citibike.getSystem('citi-bike-nyc', (err, data) => {
-  let stations = data.filter(s => config.ids.indexOf(s.id) > -1);
-  let bikes = stations.reduce((p, c) => p + c.bikes, 0);
-  console.log(stations);
-  console.log(bikes);
-});
+let sent = false;
+
+schedule('* * * * *', _ => {
+  citibike.getSystem('citi-bike-nyc', (err, data) => {
+    let stations = data.filter(s => config.ids.indexOf(s.id) > -1);
+    let bikes = stations.reduce((p, c) => p + c.bikes, 0);
+    console.log(bikes);
+    if (bikes < 10 && !sent) {
+      text(bikes + ' bikes left downtown');
+      sent = true;
+    }
+  });
+})
